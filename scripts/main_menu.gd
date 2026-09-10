89241d4dcd6716879e8922888e49709ff1db1d37
Greenbeansoup <successparrots@gmail.com>
2026-09-10 11:48:16 -0500

    Adding test arena option
    Let's me set up various tests adding menu option to allow others to see

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,14 +2,19 @@ extends Control
 
 @onready var start = $VBoxContainer/Start
 @onready var exit = $VBoxContainer/Exit
+@onready var start_test_arena = $VBoxContainer/StartTestArena
 
 func _ready():
 	start.pressed.connect(_on_start_pressed)
 	exit.pressed.connect(_on_exit_pressed)
+	start_test_arena.pressed.connect(_on_start_test_arena_pressed)
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://levels/game.tscn")
+	
+func _on_start_test_arena_pressed():
+	get_tree().change_scene_to_file("res://levels/TestArena.tscn")
 
 
 func _on_exit_pressed():
